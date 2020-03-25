6427232f5ad45a1bbbe24d352ee4d87fdb29a422
Jason Chien <jason221017@gmail.com>
2020-03-25 23:06:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/jason/Hello.kt
@@ -0,0 +1,13 @@
+package com.jason
+
+fun main() {
+//    println("Hello kotlin")
+    val h = Human();
+    h.hello()
+}
+
+class Human{
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
