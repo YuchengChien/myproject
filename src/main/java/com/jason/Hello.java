4d0dbab5dabf52cd89de959b937c9c9f089b0385
jason.chien <Jason_Chien@quantatw.com>
2020-03-26 12:16:30 +0800

    java and kotlin constructor

--- src/main/java/com/jason/Hello.java
+++ src/main/java/com/jason/Hello.java
@@ -3,13 +3,14 @@ package com.jason;
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
-        Person p = new Person();
+        Person p = new Person("Jason",66.5f, 1.7f);
         p.hello();
-        int age = 19;
+        System.out.println(p.bmi());
+        /*int age = 19;
         Integer age2 = 19;
         char c = 'A';
         byte b = 120;
         float weight = 66.5f;
-        float height = 1.7f;
+        float height = 1.7f;*/
     }
 }
--- src/main/kotlin/com/jason/Hello.kt
+++ src/main/kotlin/com/jason/Hello.kt
@@ -2,14 +2,25 @@ package com.jason
 
 fun main() {
 //    println("Hello kotlin")
-    val h = Human();
+    val h = Human(weight = 66.5f, height = 1.7f);
     h.hello()
-    var age = 19
+    println(h.bmi())
+    /*var age = 19
     var name: String = "Jason"
-    println("$name:$age")
+    println("$name:$age")*/
 }
 
-class Human{
+class Human(var name : String = "", var weight :  Float, var height : Float){
+    init {
+        println("test $weight")
+    }
+
+//    constructor(name: String, weight: Float, height: Float) : this(weight, height)
+
+    fun bmi() : Float {
+        val bmi = weight / (height * height);
+        return bmi;
+    }
     fun hello() {
         println("Hello kotlin")
     }
